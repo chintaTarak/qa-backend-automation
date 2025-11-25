ed747945912252060785b0bd29b2e9c18f6e8d6d
yogeshwarKumar <yogeshwarkumar@changejar.in>
2025-11-25 14:57:48 +0530

    Fixed Test Mobile Number (#8)
    added phone number for OTP

--- src/test/java/testData/Auth/TestDataAuth.java
+++ src/test/java/testData/Auth/TestDataAuth.java
@@ -6,6 +6,6 @@ public class TestDataAuth
     public static final String PHONE_NUMBER_ADMIN = "9999911111";
     public static final String OTP = "123456";
     public static final String ADVERTISING_ID = "test";
-    public static final String PHONE_NUMBER = "20001200051";
+    public static final String PHONE_NUMBER = "2000120005";
     public static final String ADMIN_TOKEN = "Basic cGFydG5lcl9pbmNlabnRpdmVfc2VydmljZTo1OWMyNTUxZmU0YmVmYjA4OTVlN2I3ZGI5NzMzNWRhMDRiMmI0NTlmZWViZWZlZWYxMGI5YTlkZDdlMjU3MmZi";
 }
